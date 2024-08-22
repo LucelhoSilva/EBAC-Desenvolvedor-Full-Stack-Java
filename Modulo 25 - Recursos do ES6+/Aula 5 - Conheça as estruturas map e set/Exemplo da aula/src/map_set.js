0e37866a554306f0432a3e2378805565279d89d8
Lucelho Cristiano Vieira Da Silva <lucelhocristiano28@gmail.com>
2024-08-22 08:08:15 -0300

    :pencil: Modulo 25 - Recursos do ES6+

---
+++ Modulo 25 - Recursos do ES6+/Aula 5 - Conheça as estruturas map e set/Exemplo da aula/src/map_set.js
@@ -0,0 +1,5 @@
+let meuMap = new Map(); // Cria um novo Map
+
+meuMap.set('nome', 'Lucelho'); // Adiciona um novo par chave-valor (novo item) ao Map
+
+console.log(meuMap.get('nome')); // Exibe o valor associado à chave 'nome'
